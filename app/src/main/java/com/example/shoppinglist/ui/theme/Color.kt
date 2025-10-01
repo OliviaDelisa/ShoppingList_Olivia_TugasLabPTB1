ede6ccbbfbe6dacea7e211e7f3365dbd290ac73a
OliviaDelisa <oliviadelisa123@gmail.com>
2025-10-01 20:22:25 +0700

    baru

---
+++ app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.example.shoppinglist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+
+val RedLight = Color(0xFF3346B1)
+val RedDark = Color(0xFF0C82EC)
+val RedOn = Color(0xFFFFFFFF)
+val RedBackground = Color(0x3BB1D9FF)
+val RedOnBackground = Color(0xFF1C1B1F)
+val RedSurface = Color(0xFF8DCCFF)
+val RedOnSurface = Color(0xFF050E3B)
+
+val RedSurfaceDark = Color(0xFF2B2930)
+val RedOnSurfaceDark = Color(0xFFE6E1E5)
+
+val RedSecondary = Color(0xFFFF5722)
+val RedSecondaryContainer = Color(0xFFFFEB3B)
+val RedOnSecondaryContainer = Color(0xFF02172F)
+
+val RedPrimaryContainer = Color(0xFFD6E5FF)
+val RedOnPrimaryContainer = Color(0xFF3F51B5)
+
+// Warna untuk dark mode
+val RedDarkBackground = Color(0xFF121212)
+val RedDarkOnBackground = Color(0xFFE6E1E5)
+val RedDarkPrimaryContainer = Color(0xFF6BBBFF)
+val RedDarkOnPrimaryContainer = Color(0xFFFFDAD6)
+val RedDarkSecondaryContainer = Color(0xFF73B7FF)
+val RedDarkOnSecondaryContainer = Color(0xFFFFDAD6)
